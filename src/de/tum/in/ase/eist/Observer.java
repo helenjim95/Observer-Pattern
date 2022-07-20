f0e9e276234270fab31d3c6d8102d3c46ef8320f
Lucas Welscher <lucas.welscher@tum.de>
2022-07-20 15:21:07 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/eist/Observer.java
@@ -0,0 +1,5 @@
+package de.tum.in.ase.eist;
+
+public interface Observer<T> {
+    // TODO realize observer pattern
+}
---
+++ src/de/tum/in/ase/eist/Subject.java
@@ -0,0 +1,7 @@
+package de.tum.in.ase.eist;
+
+import java.util.*;
+
+public abstract class Subject<T> {
+    // TODO realize observer pattern
+}
